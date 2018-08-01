3f3d640d672cc53f3196ffe6aff92b0f79a1a40c
Lucas Prestes <lucas.prestes.lp@gmail.com>
2018-08-01 13:57:03 -0300

    Update Scripts.R

--- Script.R
+++ Script.R
@@ -3,20 +3,27 @@
 #Try it Online!
     # http://rextester.com/l/r_online_compiler
 
-cities <- 10
+cities <- 20
 rotesPerSecond <- 1000000
   
   
 for(i in 1:cities){
   possibilities <- factorial(i)
-  pos <- c(pos, possibilities)
-  time <- possibilities/rotesPerSecond
-    
-  time <- time/60 #seconds
-  time <- time/60 #minutes
-  time <- time/60 #hours
-  time <- time/24 #days
-  time <- time/365 #years
-    
-  print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",time," years",sep=""));
+  seconds <- possibilities/rotesPerSecond
+  minutes <- seconds/60 
+  hours <- minutes/60
+  days <- hours/24
+  years <- days/365
+        
+  if(years > 1) {
+    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",years," years",sep=""));
+  } else if(days > 1) { 
+    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",days," days",sep=""));
+  } else if(hours > 1) {
+    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(hours,3)," hours",sep=""));
+  } else if(minutes > 1) {
+    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(minutes,3)," minutes",sep=""));
+  } else {
+    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(seconds,3)," seconds",sep=""));
+  }
 }
