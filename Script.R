560326fb4cdb8ac1cd1f2454df308d7a8a4bbdae
Lucas Prestes <lucas.prestes.lp@gmail.com>
2018-08-01 16:08:55 -0300

    Update Script.R

--- Script.R
+++ Script.R
@@ -3,30 +3,38 @@
 #Try it Online!
     # http://rextester.com/l/r_online_compiler
 
-cities <- 20
-rotesPerSecond <- 1000000
-  
+
+cities <- 25
+
+#CPU informations
+ghz <- 2.5
+cores <- 2
+
+#Number of CPU able to compute
+parallelCPUs <- 1
   
 for(i in 1:cities){
-  possibilities <- factorial(i)
-  seconds <- possibilities/rotesPerSecond
-  minutes <- seconds/60 
-  hours <- minutes/60
-  days <- hours/24
-  years <- days/365
-  myears <- years/1000000
+    instructionNumber <- i  #number of instruction will need to generate one rote
+    rotesPerSecond <- (ghz * 1000000 * cores * parallelCPUs) / instructionNumber
+    possibilities <- factorial(i)
+    seconds <- possibilities/rotesPerSecond
+    minutes <- seconds/60 
+    hours <- minutes/60
+    days <- hours/24
+    years <- days/365
+    myears <- years/1000000
         
-  if(myears > 1){
-    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(myears,3)," million of years",sep=""));
-  }else if(years > 1) {
-    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(years,3)," years",sep=""));
-  } else if(days > 1) { 
-    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(days,3)," days",sep=""));
-  } else if(hours > 1) {
-    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(hours,3)," hours",sep=""));
-  } else if(minutes > 1) {
-    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(minutes,3)," minutes",sep=""));
-  } else {
-    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(seconds,3)," seconds",sep=""));
-  }
+    if(myears > 1){
+        print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(myears,3)," million of years",sep=""));
+    }else if(years > 1) {
+        print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(years,3)," years",sep=""));
+    } else if(days > 1) { 
+        print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(days,3)," days",sep=""));
+    } else if(hours > 1) {
+        print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(hours,3)," hours",sep=""));
+    } else if(minutes > 1) {
+        print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(minutes,3)," minutes",sep=""));
+    } else {
+        print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(seconds,3)," seconds",sep=""));
+    }
 }
