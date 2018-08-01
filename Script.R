777ab02f8405c7cad67342b7e868d6a8bafd1348
Lucas Prestes <lucas.prestes.lp@gmail.com>
2018-08-01 14:08:57 -0300

    Update Script.R

--- Script.R
+++ Script.R
@@ -15,10 +15,12 @@ for(i in 1:cities){
   days <- hours/24
   years <- days/365
         
-  if(years > 1) {
-    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",years," years",sep=""));
+  if(myears > 1){
+    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(myears,3)," million of years",sep=""));
+  }else if(years > 1) {
+    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(years,3)," years",sep=""));
   } else if(days > 1) { 
-    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",days," days",sep=""));
+    print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(days,3)," days",sep=""));
   } else if(hours > 1) {
     print(paste("N. Cities: ",i,"   N. possibilities: ",possibilities,"   time: ",round(hours,3)," hours",sep=""));
   } else if(minutes > 1) {
